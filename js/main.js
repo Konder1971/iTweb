e8c4e8ad271f5d8f466847ff51ffefe8d8998a3d
Konder1971 <konder1971@mail.ru>
2023-04-03 00:11:23 +0300

    aeswrew
    ewreqw

--- js/main.js
+++ js/main.js
@@ -66,6 +66,26 @@
 			}
 		};
 	});
+	
+
+
+	document.addEventListener('DOMContentLoaded', () => {
+		let toTopBtn = document.querySelector('.to-up');
+		window.onscroll = function () {
+			if (window.pageYOffset > 300) {
+				toTopBtn.style.display = 'block'
+			} else {
+				toTopBtn.style.display = 'none'
+			}
+		}
+		toTopBtn.addEventListener('click', function () {
+			window.scrollBy({
+				top: -document.documentElement.scrollHeight,
+				behavior: 'smooth'
+			});
+		});
+	});
+
 
 
 /*
