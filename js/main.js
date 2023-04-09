d353a6242ddad8f7e10ec71b11ab78d3fd500a35
Konder1971 <konder1971@mail.ru>
2023-04-09 23:08:20 +0300

    dsfads
    dfadsf

--- js/main.js
+++ js/main.js
@@ -18,6 +18,41 @@
 	});
 
 
+	let top = document.querySelector('.top');
+	window.addEventListener('scroll', checkScroll);
+	document.addEventListener('DOMContentloaded', checkScroll); // чтобы не было скачка цвета при перезагрузки страницы на скролле
+	function checkScroll() {
+		let scrollPos = window.scrollY;
+		if($(window).width() > 1024) {
+			if (scrollPos > 0) {
+				top.style.cssText = `
+					height: 90px;
+				`
+			}
+			else {
+				top.style.cssText = ''
+			}
+		} else {
+			top.style.cssText = ''
+		}
+	}
+	$(window).resize(function() {
+			if($(window).width() > 1024) {
+				if (scrollPos > 0) {
+					top.style.cssText = `
+						height: 90px;
+					`
+				}
+				else {
+					top.style.cssText = ''
+				}
+			} else {
+				top.style.cssText = ''
+			}
+	});
+
+
+
 	let parent = document.querySelectorAll('.navlink')
 	//let linkdown = document.querySelectorAll('.dropdown')
 	if($(window).width() < 1025) {
@@ -86,82 +121,9 @@
 	});
 
 
-
-/*
-	$(window).on("load", function() {
-		$('.ourteamPopap').css('height', '');
-		$('.ourteamPopap').css({
-			minHeight: $('body').outerHeight(true) + 'px'
-		});
-	});
-	$(window).resize(function () {
-		$('.ourteamPopap').css('height', '');
-		$('.ourteamPopap').css({
-			minHeight: $('body').outerHeight(true) + 'px'
-		});
-	});
-*/
-	// popup .ourteam.photo
-	// $('.ourteam .photo').css('height', '');
-	// $('.ourteam .photo').css({
-	// 	height: $('.ourteam .photo').width() + 'px'
-	// });
-	// $(window).resize(function () {
-	// 	$('.ourteam .photo').css('height', '');
-	// 	$('.ourteam .photo').css({
-	// 		height: $('.ourteam .photo').width() + 'px'
-	// 	});
-	// });
-
-	// $('.ourteamPopap .photo').css('height', '');
-	// $('.ourteamPopap .photo').css({
-	// 	height: $('.ourteamPopap .photo').width() + 'px'
-	// });
-	// $(window).resize(function () {
-	// 	$('.ourteamPopap .photo').css('height', '');
-	// 	$('.ourteamPopap .photo').css({
-	// 		height: $('.ourteamPopap .photo').width() + 'px'
-	// 	});
-	// });
-
-
-	// $('.personDetails').click(function () {
-	// 	//document.body.style.cssText += `overflow:hidden`;
-	// 	var id = $(this).data('id');
-	// 	if ($(this).find('.photo').hasClass('boardAdvisors-photo')) {
-	// 		$('.ourteamPopap .photo').addClass('boardAdvisors-photo');
-	// 	} else {
-	// 		$('.ourteamPopap .photo.boardAdvisors-photo').removeClass('boardAdvisors-photo');
-	// 	}
-	// 	$('.fonPopap').addClass('active');
-	// 	$('.popup'+id).addClass('active');
-	// 	return false;
-	// });
-	// $('.ourteamPopap .close').click(function () {
-	// 	//document.body.style.cssText = `overflow: auto;`;
-	// 	$('.fonPopap.active').removeClass('active');
-	// 	$('.ourteamPopap.active').removeClass('active');
-	// 	return false;
-	// });
-
-    if($(".select").length > 0){
-			$(".select").selectmenu();
-		}
-
-	/*
-	$(window).scroll(function () {
-		if($('body').hasClass('home')){
-			var headerShift = $('.header').height()+50;
-			var bodyPadding = $('body').css('padding-top');
-			if ($(this).scrollTop() > headerShift) {
-				$('.header').addClass('homeFixed');
-				$('body').css('padding-top', bodyPadding*1+headerShift*1);
-			} else {
-				$('.header').removeClass('homeFixed');
-			}
-		}
-	});
-	*/
+	if($(".select").length > 0){
+		$(".select").selectmenu();
+	}
 
 
 	function isMac() {
