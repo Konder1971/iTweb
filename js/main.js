67079a5b553df32a9201bf3ae1c2ed3b806e0d65
Konder <konder1971@mail.ru>
2025-03-05 21:56:10 +0200

    dfsdaf
    dfdsf

--- js/main.js
+++ js/main.js
@@ -75,24 +75,6 @@
 			}
 		};
 	});
-	
-
-	document.addEventListener('DOMContentLoaded', () => {
-		let toTopBtn = document.querySelector('.to-up');
-		window.onscroll = function () {
-			if (window.pageYOffset > 400) {
-				toTopBtn.style.display = 'block'
-			} else {
-				toTopBtn.style.display = 'none'
-			}
-		}
-		toTopBtn.addEventListener('click', function () {
-			window.scrollBy({
-				top: -document.documentElement.scrollHeight,
-				behavior: 'smooth'
-			});
-		});
-	});
 
 
 	if($(".select").length > 0){
@@ -115,3 +97,21 @@ function isiOS() {
 }
 
 })(jQuery);
+	
+
+document.addEventListener('DOMContentLoaded', () => {
+  let toTopBtn = document.querySelector('.to-up');
+  window.onscroll = function () {
+    if (window.pageYOffset > 400) {
+      toTopBtn.style.display = 'block'
+    } else {
+      toTopBtn.style.display = 'none'
+    }
+  }
+  toTopBtn.addEventListener('click', function () {
+    window.scrollBy({
+      top: -document.documentElement.scrollHeight,
+      behavior: 'smooth'
+    });
+  });
+});
